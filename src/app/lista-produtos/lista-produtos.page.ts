9665ed683004f92f05415aebc9bc735d7ac4e8a4
Lucas Alessandro <lucas_hra@hotmail.com>
2023-09-20 23:01:50 -0300

    Ajustando-rotas

--- src/app/lista-produtos/lista-produtos.page.ts
+++ src/app/lista-produtos/lista-produtos.page.ts
@@ -1,4 +1,5 @@
 import { Component , OnInit} from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -9,21 +10,33 @@ export class ListaProdutosPage implements OnInit{
  
   ngOnInit() {
   }
-  produtos = [
+
+  constructor (
+    private navCtrl: NavController
+  ) {}
+
+  public produtos = [
     {
+      id: 1,
       imagem: '../assets/images/mouse.jpg',
       titulo: 'Mouse', 
       descricao: 'Um dispositivo de entrada para computadores.' 
     },
     { 
+      id: 2,
       imagem: '../assets/images/teclado.webp',
       titulo: 'Teclado', 
       descricao: 'Um dispositivo de entrada para inserir texto em um computador.' 
     },
     { 
+      id: 3,
       imagem: '../assets/images/monitor.avif',
       titulo: 'Monitor', 
       descricao: 'Um dispositivo de saída que exibe informações visuais geradas por um computador.' 
     },
   ]
+
+  showDescricaoProdutos(id: any) {
+    this.navCtrl.navigateForward("descricao-produtos/id/"+id);
+  }
 }
